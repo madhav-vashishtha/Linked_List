5bb9e5cbc77c608f0b6972a2552f92b980fe1eb2
madhav-vashishtha <madhavvash1105@gmail.com>
2026-07-11 21:33:56 +0530

    Insert at Tail Problem

---
+++ Insert_in_LL_II.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self,value):
+        self.data = value
+        self.next = None
+
+def print_LL(head):
+    temp = head
+    while temp!=None:
+        print(temp.data,end="->")
+        temp = temp.next
+
+    print()
+    return
+
+def take_input():
+    value = int(input("Enter the value of Node: "))
+    head = None
+
+    while value != -1:
+        newNode = Node(value)
+        if head == None:
+            head = newNode
+        else:
+            temp = head
+            while temp.next!=None:
+                temp=temp.next
+
+            temp.next = newNode
+
+        value = int(input("Enter the value of Node: "))
+
+    return head
+
+def insert_at_tail(head,data):
+    newNode = Node(data)
+    if(head is None):
+        return newNode
+    
+    temp = head
+    while(temp.next != None):
+        temp = temp.next
+
+    temp.next = newNode
+
+    return head
+
+head = take_input()
+
+head = insert_at_tail(head,45)
+print("After Inserting at Tail")
+print_LL(head)
